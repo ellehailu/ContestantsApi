c320a158baf4a4b548266c518d1829f6d9a3650c
ellehailu <hailu.elle@gmail.com>
2023-09-29 15:21:31 -0700

    add post method to playerContestant controller

--- Controllers/PlayerContestantController.cs
+++ Controllers/PlayerContestantController.cs
@@ -2,25 +2,46 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ContestantsApi.Models;
 
-namespace ContestantsApi.Controllers
-{
+namespace ContestantsApi.Controllers{
     [Route("api/[controller]")]
     [ApiController]
 
-    public class PlayerContestantController : ControllerBase
-    {
+    public class PlayerContestantController : ControllerBase{
         private readonly ContestantsApiContext _db;
 
-        public PlayerContestantController(ContestantsApiContext db)
-        {
+        public PlayerContestantController(ContestantsApiContext db){
             _db = db;
         }
 
-
-        //GET api/JointTable
+        // Get api/playerContestant
         [HttpGet]
-        public async Task<IEnumerable<PlayerContestant>> get() {
+        public async Task<ActionResult<IEnumerable<PlayerContestant>>> Get(){
             return await _db.PlayersContestants.ToListAsync();
         }
+
+        // Get api/playerContestant/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PlayerContestant>> GetPlayerContestant(int id){
+            PlayerContestant playerContestant = await _db.PlayersContestants.FindAsync(id);
+
+            if (playerContestant == null){
+                return NotFound();
+            }
+
+            return playerContestant;
+        }
+
+
+        // Create entries for playerContestant
+        // POST api/playerContestant
+        [HttpPost]
+        public async Task<ActionResult<PlayerContestant>> Post([FromBody] PlayerContestant playerContestant){
+            _db.PlayersContestants.Add(playerContestant);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction("GetPlayerContestant", new { id = playerContestant.PlayerContestantID }, playerContestant);
+        }
+
+        //
     }
 }
